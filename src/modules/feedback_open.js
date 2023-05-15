cc2fa8df15a8afd3bd56811347ee21240b47d782
Anton Maslikov <totosa9494@gmail.com>
2023-05-15 15:04:32 +0300

    disabled module window

--- src/modules/feedback_open.js
+++ src/modules/feedback_open.js
@@ -1,25 +1,29 @@
-let feedbackBlock = document.querySelector('.feedback');
-let feedbackContainer = document.querySelector('.feedback-container');
-let feedbackMenuOpenButton = document.querySelector('.action-buttons__button--feedback');
-let feedbackNavigationOpenButton = document.querySelector('.navigation__button--chat');
-let feedbackCloseButton = document.querySelector('.feedback-header__button');
+let feedbackBlock = document.querySelector(".feedback")
+let feedbackContainer = document.querySelector(".feedback-container")
+let feedbackMenuOpenButton = document.querySelector(
+  ".action-buttons__button--feedback"
+)
+let feedbackNavigationOpenButton = document.querySelector(
+  ".navigation__button--chat"
+)
+let feedbackCloseButton = document.querySelector(".feedback-header__button")
 
-feedbackMenuOpenButton.addEventListener('click', () => {
-	feedbackBlock.classList.add('feedback--animation-open');
-	feedbackContainer.classList.add('feedback-container--open');
-});
+feedbackMenuOpenButton.addEventListener("click", () => {
+  feedbackBlock.classList.add("feedback--animation-open")
+  feedbackContainer.classList.add("feedback-container--open")
+})
 
-feedbackNavigationOpenButton.addEventListener('click', () => {
-	feedbackBlock.classList.add('feedback--animation-open');
-	feedbackContainer.classList.add('feedback-container--open');
-});
+feedbackNavigationOpenButton.addEventListener("click", () => {
+  feedbackBlock.classList.add("feedback--animation-open")
+  feedbackContainer.classList.add("feedback-container--open")
+})
 
-feedbackCloseButton.addEventListener('click', () => {
-	feedbackBlock.classList.remove('feedback--animation-open');
-	feedbackContainer.classList.remove('feedback-container--open');
-});
+feedbackCloseButton.addEventListener("click", () => {
+  feedbackBlock.classList.remove("feedback--animation-open")
+  feedbackContainer.classList.remove("feedback-container--open")
+})
 
-feedbackContainer.addEventListener('click', () => {
-	feedbackBlock.classList.remove('feedback--animation-open');
-	feedbackContainer.classList.remove('feedback-container--open');
-});
+// feedbackContainer.addEventListener('click', () => {
+// 	feedbackBlock.classList.remove('feedback--animation-open');
+// 	feedbackContainer.classList.remove('feedback-container--open');
+// });
